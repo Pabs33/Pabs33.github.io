17b3954ef294f5a10f7cfa41a5fb857f59d5ea96
Pablobalmis <pablomartinezaznar@gmail.com>
2021-06-17 12:25:13 +0200

    Vamos a ver si funciona

--- Script.js
+++ Script.js
@@ -1,13 +1,36 @@
+
+
 LoadLDocA("ToDoList.txt", "txt");
 
 function leerTXT(fichero)
 {
     let lineas = fichero.split("\n");
-    let centro = document.querySelector(".centro");
-    cosas = "";
+    let centro = document.querySelector(".Proyectos");
+    let cosas = "";
     for(let i = 0; i < lineas.length; i++)
     {
         cosas += "<div class='Nombre'>" + lineas[i] + "</div>";
     }
     centro.innerHTML = cosas;
+}
+function ReocgerDatos()
+{
+    let proyecto = document.getElementById("proyecto").value;
+    escribirTexto(proyecto)
+}
+function escribirTexto(proyecto)
+{
+    /*
+    let fs = require('fs');
+    fs.writeFile(proyecto);*/
+
+    /*
+    var fso  = FileHandle("Scripting.FileSystemObject"); 
+    var fh = fso.OpenTextFile("ToDoList.txt", 2, false); 
+    fh.WriteLine(proyecto); 
+    fh.Close(); */
+
+    let fso = CreateObject("Scripting.FileSystemObject"); 
+    let s = fso.CreateTextFile("<your Path>/filename.txt", True);
+    s.writeline(proyecto);
 }
